8a7a49bc90156566c7b36885203e8cbaaad60aa7
Kevin Takano <kevin.takano@gmail.com>
2023-07-15 16:06:59 -0400

    Initial commit

---
+++ definitions.py
@@ -0,0 +1,8 @@
+import os
+
+# This is your Project Root
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# data dirs
+RAW_DATA_DIR = os.path.join(ROOT_DIR, "data", "raw")
+PROCESSED_DATA_DIR = os.path.join(ROOT_DIR, "data", "processed")
